a842291759fe1780c9d1bbc4dbe2b76525b782e1
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-09 22:21:08 +0600

    username added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import app from './firebase.init';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
@@ -10,6 +10,7 @@ const auth = getAuth(app)
 const App = () => {
     const [email, setEmail]=useState('')
     const [password, setPassword]=useState('')
+    const [name, setName]=useState('')
     const [validated, setValidated] = useState(false);
     const [error, setError]=useState('')
     const [resister, setResister]=useState(false)
@@ -54,6 +55,7 @@ const App = () => {
                 setEmail('')
                 setPassword('')
                 verifyEmail()
+                setUserName()
             })
             .catch(error=>{
                 console.log(error)
@@ -62,11 +64,25 @@ const App = () => {
         }
         event.preventDefault()
     }
+    const setUserName=()=>{
+        updateProfile(auth.currentUser, {
+           displayName:name 
+        })
+        .then(()=>{
+            console.log('updating name')
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
     const verifyEmail=()=>{
         sendEmailVerification(auth.currentUser)
         .then(()=>{
             console.log('email verification sent')
         })
+    }
+    const handelNameBlur=event=>{
+       setName(event.target.value)
     }
     const handleResisteChange=(event)=>{
         setResister(event.target.checked)
@@ -81,6 +97,14 @@ const App = () => {
     <div className='w-50 mx-auto'>
         <h2 className='text-primary'>plese {resister? 'Login':'register'}</h2>
         <Form noValidate validated={validated} onSubmit={handleformSubmit}>
+            { !resister && <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>your name</Form.Label>
+                <Form.Control onBlur={handelNameBlur} type="text" placeholder="your name" required />
+                
+                <Form.Control.Feedback type="invalid">
+                Please provide your name.
+                </Form.Control.Feedback>
+            </Form.Group>}
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter email" required />
